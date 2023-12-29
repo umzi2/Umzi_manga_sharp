0a4d5238f5c011032eaf5a03c6f5e5a420bcc0ff
umzi2 <ab5447612@gmail.com>
2023-12-29 20:30:35 +0300

    fix:code

--- main.py
+++ main.py
@@ -1,54 +1,14 @@
-import cv2
-import os
-import numpy as np
+from concurrent.futures import ThreadPoolExecutor
 import concurrent.futures
-import configparser
+import json
+import os
 from time import time as ttime
+from tqdm import tqdm
+import cv2
+import numpy as np
+import argparse
 
-image_folder = 'INPUT'
-output_folder = 'SHARP'
 
-config_file_path = "config.ini"
-t0 = ttime()
-default_config = {
-    "low_input": "4",
-    "high_input": "255",
-    "low_output": "0",
-    "high_output": "255",
-    "gamma": "1.0",
-    "diapason_black": "14",
-    "diapason_white": "10"
-}
-def read_config():
-    if os.path.exists(config_file_path):
-        config = configparser.ConfigParser()
-        config.read(config_file_path)
-        diapason_white = int(config.get("Settings", "diapason_white"))
-        low_input = int(config.get("Settings", "low_input"))
-        high_input = int(config.get("Settings", "high_input"))
-        low_output = int(config.get("Settings", "low_output"))
-        high_output = int(config.get("Settings", "high_output"))
-        gamma = float(config.get("Settings", "gamma"))
-        diapason = int(config.get("Settings", "diapason_black"))
-    else:
-        diapason_white = int(default_config["diapason_white"])
-        low_input = int(default_config["low_input"])
-        high_input = int(default_config["high_input"])
-        low_output = int(default_config["low_output"])
-        high_output = int(default_config["high_output"])
-        gamma = float(default_config["gamma"])
-        diapason = int(default_config["diapason_black"])
-        config = configparser.ConfigParser()
-        config["Settings"] = default_config
-        with open(config_file_path, "w") as configfile:
-            config.write(configfile)
-    return  diapason_white, low_input, high_input, low_output, high_output, gamma, diapason
-
-if not os.path.exists(output_folder):
-    os.makedirs(output_folder)
-if not os.path.exists(image_folder):
-    os.makedirs(image_folder)
-diapason_white, low_input, high_input, low_output, high_output, gamma, diapason = read_config()
 def process_image(filename):
     image_path = os.path.abspath(os.path.join(image_folder, filename))
     if not os.path.exists(image_path):
@@ -63,25 +23,72 @@ def process_image(filename):
         print(f"Не удалось загрузить изображение {image_path}. Пропуск файла.")
         return
     gray = cv2.cvtColor(umzi, cv2.COLOR_RGB2GRAY)
-    gray2 = cv2.medianBlur(gray, 3)
-    mask2 = cv2.inRange(gray2, max(255 - diapason_white, 0), min(255 + diapason_white, 255))
-    image = np.clip(((gray.astype(np.float32) - low_input) / (high_input - low_input)) ** gamma * (high_output - low_output) + low_output, 0, 255).astype(np.uint8)
-    _, black_mask = cv2.threshold(image, diapason, 255, cv2.THRESH_BINARY)
-    blur = cv2.GaussianBlur(black_mask, (3, 3), 0)
-    output2 = np.clip(((blur.astype(np.float32) - 253) / (254 - 253)) ** 1.0 * (255 - 0) + 0, 0, 255).astype(np.uint8)
-    edges = cv2.Canny(image, 750, 800, apertureSize=3, L2gradient=True)
-    inverted_edges = cv2.bitwise_not(edges)
-    sharpened_image = cv2.bitwise_and(image, image, mask=output2)
-    result = cv2.bitwise_and(sharpened_image, sharpened_image, mask=inverted_edges)
-    final_result = cv2.add(result, mask2)
+
+    image = np.clip(((gray.astype(np.float32) - configs.get("low_input")) / (
+            configs.get("high_input") - configs.get("low_input"))) ** configs.get("gamma") * (
+                            configs.get("high_output") - configs.get("low_output")) + configs.get("low_output"), 0,
+                    255).astype(np.uint8)
+    if configs.get("diapason_black") != -1:
+        _, black_mask = cv2.threshold(image, configs.get("diapason_black"), 255, cv2.THRESH_BINARY)
+        blur = cv2.GaussianBlur(black_mask, (3, 3), 0)
+
+        output2 = np.clip(((blur.astype(np.float32) - 253) / (254 - 253)) ** 1.0 * (255 - 0) + 0, 0, 255).astype(
+            np.uint8)
+        image = cv2.bitwise_and(image, image, mask=output2)
+
+    if configs.get("cenny"):
+        edges = cv2.Canny(image, 750, 800, apertureSize=3, L2gradient=True)
+        inverted_edges = cv2.bitwise_not(edges)
+
+        image = cv2.bitwise_and(image, image, mask=inverted_edges)
+
+    if configs.get("diapason_white") != -1:
+        gray2 = cv2.medianBlur(gray, 3)
+        mask2 = cv2.inRange(gray2, max(255 - configs.get("diapason_white"), 0),
+                            min(255 + configs.get("diapason_white"), 255))
+        image = cv2.add(image, mask2)
+
     output_path = os.path.join(output_folder, filename)
-    cv2.imwrite(output_path, final_result)
-    print(f"Результат сохранён{output_path}")
-image_files = [f for f in os.listdir(image_folder) if os.path.isfile(os.path.join(image_folder, f)) and not f.startswith('.')]
-num_images = len(image_files)
-num_cores = os.cpu_count() if os.cpu_count() is not None else 1
-num_workers = min(num_images, num_cores)
-with concurrent.futures.ThreadPoolExecutor(max_workers=num_workers) as executor:
-    executor.map(process_image, image_files)
-t1 = ttime()
-print("time_spent", t1 - t0)
+    cv2.imwrite(output_path, image)
+
+
+def process_image_with_progress(filename):
+    process_image(filename)
+    pbar.update(1)  # Обновление прогресс-бара
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Image processing script")
+    parser.add_argument("--input_folder", type=str, default='INPUT',
+                        help="Path to the input image folder")
+    parser.add_argument("--output_folder", type=str, default='SHARP',
+                        help="Path to the output image folder")
+
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    image_folder = args.input_folder
+    output_folder = args.output_folder
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+    if not os.path.exists(image_folder):
+        os.makedirs(image_folder)
+    with open('config.json') as f:
+        configs = json.load(f)
+
+    image_files = [f for f in os.listdir(image_folder) if
+                   os.path.isfile(os.path.join(image_folder, f)) and not f.startswith('.')]
+    num_images = len(image_files)
+    num_cores = os.cpu_count() if os.cpu_count() is not None else 1
+
+    t0 = ttime()
+    with tqdm(total=num_images, desc="Processing images") as pbar:
+        with ThreadPoolExecutor(max_workers=num_cores) as executor:
+            futures = [executor.submit(process_image_with_progress, filename) for filename in image_files]
+            for future in concurrent.futures.as_completed(futures):
+                future.result()
+    t1 = ttime()
+
+    print("Time spent:", t1 - t0)
